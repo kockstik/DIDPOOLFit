4665a12c31c4693e4cf2bfed38572e5e07afa54e
Kockstik <kostyadi04@mail.ru>
2026-05-18 17:53:19 +0700

    Initial Commit

---
+++ DIDPOOLFit/DIDPOOLFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIDPOOLFitApp.swift
+//  DIDPOOLFit
+//
+//  Created by admin on 18.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct DIDPOOLFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
