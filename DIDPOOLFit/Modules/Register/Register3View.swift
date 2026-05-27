85c85ac9389f6eb85a61af2c6d7afed0ba94be7a
Kockstik <kostyadi04@mail.ru>
2026-05-27 22:55:17 +0700

    Create view

---
+++ DIDPOOLFit/Modules/Register/Register3View.swift
@@ -0,0 +1,14 @@
+//
+//  Register3View.swift
+//  DIDPOOLFit
+//
+//  Created by admin on 27.05.2026.
+//
+
+import SwiftUI
+
+struct Register3View: View {
+    var body: some View {
+            
+    }
+}
