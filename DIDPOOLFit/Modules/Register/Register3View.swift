10b4d65d1449aa65cfeb6a2f359efcd535b47e0e
Kockstik <kostyadi04@mail.ru>
2026-05-29 21:29:18 +0700

    Create view

--- DIDPOOLFit/Modules/Register/Register3View.swift
+++ DIDPOOLFit/Modules/Register/Register3View.swift
@@ -9,6 +9,32 @@ import SwiftUI
 
 struct Register3View: View {
     var body: some View {
+        VStack{
+            VStack(spacing: 5){
+                Text("What is your goal ?")
+                    .font(.title).bold()
+                    .foregroundStyle(.appBlack)
+                Text("It will help us to choose a best\nprogram for you")
+                    .font(.footnote)
+                    .foregroundStyle(.gray1)
+                    .multilineTextAlignment(.center)
+            }
             
+            GoalSelectionView()
+                .padding(.top, 50)
+                .padding(.horizontal, -30)
+            
+            Spacer()
+            
+            RoundedButton(title: "Confirm")
+        }
+        .padding(.top, 50)
+        .padding(.vertical, 40)
+        .padding(.horizontal, 30)
+        .ignoresSafeArea()
     }
+}
+
+#Preview {
+    Register3View()
 }
