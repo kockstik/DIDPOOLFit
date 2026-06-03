46d039cc1b255cc1b61dbe764a2dd64f5570209c
Kockstik <kostyadi04@mail.ru>
2026-06-03 21:50:08 +0700

    Create view empty

---
+++ DIDPOOLFit/Modules/Main/Home/HomeView.swift
@@ -0,0 +1,17 @@
+//
+//  HomeView.swift
+//  DIDPOOLFit
+//
+//  Created by admin on 3.06.2026.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Rectangle()
+            .fill(.gray)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.gray)
+    }
+}
