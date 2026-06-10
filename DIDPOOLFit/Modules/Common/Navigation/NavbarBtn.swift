14f2c7bb1023d6806963ad5c569ea72f93462857
Kockstik <kostyadi04@mail.ru>
2026-06-10 19:01:42 +0700

    Create button for navigation bar

--- DIDPOOLFit/Modules/Common/Navigation/NavbarBtn.swift
+++ DIDPOOLFit/Modules/Common/Navigation/NavbarBtn.swift
@@ -5,3 +5,36 @@
 //  Created by admin on 3.06.2026.
 //
 
+import SwiftUI
+
+struct NavbarBtn: View {
+    
+    @Binding var selectedTab: Int
+    
+    var image: UIImage
+    var index: Int
+    
+    private var solidGradient: LinearGradient {
+        return LinearGradient(colors: [.gray2], startPoint: .top, endPoint: .bottom)
+    }
+    
+    var body: some View {
+        Button {
+            if selectedTab != index {
+                selectedTab = index
+            }
+        } label: {
+            VStack(spacing: 3){
+                Image(uiImage: image)
+                    .foregroundStyle(selectedTab == index ? LogoGradient.gradient : solidGradient)
+                    .scaledToFit()
+                    .frame(width: 24, height: 24)
+                
+                RoundedRectangle(cornerRadius: 99)
+                    .fill(LogoGradient.gradient)
+                    .frame(width: 4, height: 4)
+                    .opacity(selectedTab == index ? 1 : 0)
+            }
+        }
+    }
+}
