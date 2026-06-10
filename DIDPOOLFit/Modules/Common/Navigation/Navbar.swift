1338447475bc5bc681716cda682d5e6ed19d240c
Kockstik <kostyadi04@mail.ru>
2026-06-10 19:01:53 +0700

    Created navigation bar

--- DIDPOOLFit/Modules/Common/Navigation/Navbar.swift
+++ DIDPOOLFit/Modules/Common/Navigation/Navbar.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct Navbar: View {
+    @Binding var selectedTab: Int
+    
     var body: some View {
         HStack{
+            HStack(spacing: 38){
+                NavbarBtn(selectedTab: $selectedTab, image: .home, index: 0)
+                NavbarBtn(selectedTab: $selectedTab, image: .activity, index: 1)
+            }
             
+            RoundedButton(leadingImage: Image(.search)){
+                
+            }
+            .frame(width: 60)
+            .padding(.horizontal, 40)
+            .padding(.top, -30)
+            .scaleEffect(1.2)
+            
+            HStack(spacing: 38){
+                NavbarBtn(selectedTab: $selectedTab, image: .camera, index: 2)
+                NavbarBtn(selectedTab: $selectedTab, image: .user, index: 3)
+            }
         }
         .frame(height: 80)
     }
